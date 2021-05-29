a7a9522ae09a98ada70a29f659a5ed3d9fe8c7c2
Opeyemi Adenuga <nugayem@yahoo.com>
2021-05-29 22:58:41 +0100

    End of section 14

--- API/Startup.cs
+++ API/Startup.cs
@@ -13,17 +13,18 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using StackExchange.Redis;
 
 namespace API
 {
     public class Startup
     {
+        public IConfiguration _config { get; }
+
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            _config = configuration;
         }
-
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -31,8 +32,12 @@ namespace API
 
             services.AddControllers();
             
-            services.AddDbContext<StoreContext>(x=>x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
-            
+            services.AddDbContext<StoreContext>(x=>x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
+
+            services.AddSingleton<IConnectionMultiplexer>(c=>{
+                var configuration = ConfigurationOptions.Parse(_config.GetConnectionString("Redis"), true);
+                return ConnectionMultiplexer.Connect(configuration);                
+            }) ;         
         
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();
